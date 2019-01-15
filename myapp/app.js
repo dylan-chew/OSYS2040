c6e83104bc70762c9c76cc765f03e3871349fb7f
W0254970 <W0254970@AD.NET172.CA>
2019-01-15 13:53:35 -0400

    installed express basic app

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Very COOL!!!'))
+app.get('/', (req, res) => res.send('Welome to this test site very cool huh'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
